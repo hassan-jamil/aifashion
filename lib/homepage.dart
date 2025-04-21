4f4caadff8be889443eb28dd04278c084d868278
hassan-jamil <hassan22rao@gmail.com>
2025-04-21 15:59:16 +0500

    background wall

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,7 +9,7 @@ import 'profilepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Make sure Firebase is initialized
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -32,7 +32,6 @@ class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
   String? _profileImagePath;
 
-  // List of Pages for Bottom Navigation
   final List<Widget> _pages = [
     HomeContent(),
     AIFashionPage(),
@@ -43,10 +42,9 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    _loadProfileImage(); // Load the profile image when the page initializes
+    _loadProfileImage();
   }
 
-  // Function to load profile image path from shared preferences
   Future<void> _loadProfileImage() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
@@ -90,11 +88,11 @@ class _HomePageState extends State<HomePage> {
             Spacer(),
             GestureDetector(
               onTap: () {
-                // Navigate to ProfilePage and pass the profile image path
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => ProfilePage(profileImagePath: _profileImagePath),
+                    builder: (context) =>
+                        ProfilePage(profileImagePath: _profileImagePath),
                   ),
                 );
               },
@@ -154,208 +152,147 @@ class HomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-    final String userName = user?.displayName ?? "User";
-
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // AI Outfit Recommendation Button
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Center(
-                  child: Text(
-                    "AI Outfit Recommendations",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+    return Stack(
+      children: [
+        // Background Image
+        Positioned.fill(
+          child: Image.asset(
+            'asset/bg2.jpg',
+            fit: BoxFit.fill,
+          ),
+        ),
+        // Foreground Content
+        SingleChildScrollView(
+          child: Container(
+            color: Colors.black.withOpacity(0.3), // Optional overlay for readability
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.9),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "AI Outfit Recommendations",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
+                SizedBox(height: 20),
 
-              // Brands Section
-              Text(
-                "Brands",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 120,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: brands.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SubcategoryPage(
-                              title: brands[index],
-                              images: brandSubcategories[brands[index]]!,
-                            ),
-                          ),
-                        );
-                      },
-                      child: Padding(
+                Text("Brands", style: _sectionStyle()),
+                SizedBox(height: 10),
+                _buildHorizontalList(context, brands, brandImages, brandSubcategories),
+
+                SizedBox(height: 20),
+                Text("Categories", style: _sectionStyle()),
+                SizedBox(height: 10),
+                _buildHorizontalList(context, categories, categoryImages, categorySubcategories),
+
+                SizedBox(height: 20),
+                Text("Outfit Ideas", style: _sectionStyle()),
+                SizedBox(height: 10),
+                Container(
+                  height: 200,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: outfitImages.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
                         padding: const EdgeInsets.only(right: 16),
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 80,
-                              height: 80,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                shape: BoxShape.circle,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.3),
-                                    spreadRadius: 2,
-                                    blurRadius: 5,
-                                    offset: Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: Image.asset(
-                                brandImages[index],
-                                width: 40,
-                                height: 40,
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Text(
-                              brands[index],
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 20),
-
-              // Categories Section
-              Text(
-                "Categories",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 120,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SubcategoryPage(
-                              title: categories[index],
-                              images: categorySubcategories[categories[index]]!,
-                            ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            outfitImages[index],
+                            width: 150,
+                            height: 200,
+                            fit: BoxFit.cover,
                           ),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 16),
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 80,
-                              height: 80,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                shape: BoxShape.circle,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.3),
-                                    spreadRadius: 2,
-                                    blurRadius: 5,
-                                    offset: Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: Image.asset(
-                                categoryImages[index],
-                                width: 40,
-                                height: 40,
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Text(
-                              categories[index],
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-
-              // Outfit Ideas Section
-              Text(
-                "Outfit Ideas",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: outfitImages.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 16),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset(
-                          outfitImages[index],
-                          width: 150,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
+      ],
+    );
+  }
+
+  TextStyle _sectionStyle() {
+    return TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white);
+  }
+
+  Widget _buildHorizontalList(BuildContext context, List<String> titles,
+      List<String> images, Map<String, List<String>> subcategories) {
+    return Container(
+      height: 120,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: titles.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SubcategoryPage(
+                    title: titles[index],
+                    images: subcategories[titles[index]]!,
+                  ),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Column(
+                children: [
+                  Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      images[index],
+                      width: 40,
+                      height: 40,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    titles[index],
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
